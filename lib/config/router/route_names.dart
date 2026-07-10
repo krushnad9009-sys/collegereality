55ee38bf6d2d868f1b968d23bbd40d2f37fc390a
krushnad9009-sys <krushnad9009@gmail.com>
2026-07-10 10:26:07 +0530

    feat: Add route names constants

---
+++ lib/config/router/route_names.dart
@@ -0,0 +1,11 @@
+class RouteNames {
+  static const String splash = '/';
+  static const String onboarding = '/onboarding';
+  static const String login = '/login';
+  static const String signup = '/signup';
+  static const String home = '/home';
+  static const String collegeSearch = '/college-search';
+  static const String collegeDetails = '/college-details/:id';
+  static const String profile = '/profile';
+  static const String favorites = '/favorites';
+}
